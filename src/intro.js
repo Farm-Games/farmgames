14532cede1b4e7c1bf077f660702c2d54d6e8f34
schnozchan <schnozchan@gmail.com>
2025-01-18 12:52:46 -0500

    Intro update

--- src/intro.js
+++ src/intro.js
@@ -2,10 +2,15 @@ const mountIntroScreen = () => `
 <div class="intro-outer">
     <div class="intro">
         <h2>Farm Games Wiki</h2>
-        <video controls="false" autoplay muted loop id="intro-video" src="/intro.mp4"></video>
-        <audio autoplay loop id="intro-audio" src="/intro.mp3"></audio>
+        <div class="intro-intro">
+            <button id="first-enter-btn">Ready to rock?</button>
+        </div>
+        <div class="actual-intro">
+            <video controls="false" muted loop id="intro-video" src="/intro.mp4"></video>
+            <audio loop id="intro-audio" src="/intro.mp3"></audio>
 
-        <button id="enter-btn">Enter</button>
+            <button id="enter-btn">Enter</button>
+        </div>
 
         <script>
             const milliseconds = (h, m, s) => ((h*60*60+m*60+s)*1000)
@@ -14,24 +19,48 @@ const mountIntroScreen = () => `
             const lastDismissedDate = lastDissmissedString ? new Date(Number(lastDissmissedString)) : false
             const hasNotDismissedInTheLastTwelveHours = !lastDismissedDate || (new Date().getTime() - lastDismissedDate.getTime() > twelveHours)
             const hasDismissed = !hasNotDismissedInTheLastTwelveHours
+
+            const introIntro = document.querySelector('.intro-intro');
+            const firstEnterButton = document.getElementById('first-enter-btn');
+            const actualIntro = document.querySelector('.actual-intro');
             const introVideo = document.getElementById('intro-video');
             const introAudio = document.getElementById('intro-audio');
             const enterButton = document.getElementById('enter-btn');
+
             introAudio.volume = 0.25;
 
-            const enterSite = () => {
+            const enterSite = (shouldNotSetDismissedTime) => {
                 introVideo.pause();
                 introAudio.pause();
-                localStorage.setItem("dismissed-intro", new Date().getTime());
+                if (!shouldNotSetDismissedTime) {
+                    localStorage.setItem("dismissed-intro", new Date().getTime());
+                }
                 document.querySelector('.intro-outer').style.display = 'none';
             };
 
             if (hasDismissed) {
-                enterSite();
+                enterSite(true);
             }
+
+            const startIntro = () => {
+                introVideo.play();
+                introAudio.play();
+                introAudio.volume = 0.25;
+                introVideo.addEventListener('ended', () => {
+                    introVideo.currentTime = 0;
+                    introVideo.play();
+                });
+                introAudio.addEventListener('ended', () => {
+                    introAudio.currentTime = 0;
+                    introAudio.play();
+                });
+                introIntro.style.display = 'none';
+                actualIntro.style.display = 'flex';
+            };
 
             document.addEventListener('keydown', enterSite);
             enterButton.addEventListener('click', enterSite);
+            firstEnterButton.addEventListener('click', startIntro);
         </script>
     </div>
 </div>
